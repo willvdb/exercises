71a5830359484b16d349f5cfa05571e862db61fc
willvdb <willvdb9141@gmail.com>
2016-04-21 14:24:01 -0600

    removing some more console logs

--- value/index.js
+++ value/index.js
@@ -6,39 +6,4 @@ function value(func, context){
   }
 }
 
-var scalar = 'foo';
-var fn = function() { return 'bar'; };
-var fnTwice = function() {
-  return fn;
-};
-var fnThrice = function() {
-  return fnTwice;
-};
-
-var whoa = function() {
-  return function() {
-    return function() {
-      return function() {
-        return function() {
-          return function() {
-            return function() {
-              return function() {
-                return function() {
-                  return 'hi';
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
-
-console.log(value(scalar));   // should be      'foo'
-console.log(value(fn));       // should be      'bar'
-console.log(value(fnTwice));  // should also be 'bar'
-console.log(value(fnThrice)); // should also be 'bar'
-console.log(value(whoa)); // should be      'hi'
-
 module.exports = value;
